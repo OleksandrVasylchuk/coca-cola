909e4234c8323a8a3db44ceb1684a318f33c2937
OleksandrVasylchuk <sanyabatyushka@gmail.com>
2022-02-01 02:15:12 +0200

    fix button

--- js/index.js
+++ js/index.js
@@ -6,59 +6,28 @@ window.onload = function () {
   }, 500);
 };
 
-// Scroll!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+function burgerMenu(selector) {
+  let menu = document.querySelector(selector);
+  let button = menu.querySelector(".burger-menu_button");
+  let links = menu.querySelector(".burger-menu_link");
+  let body = document.querySelector("body");
 
-// var myFullpage = new fullpage("#fullpage", {
-//   css3: true,
-//   scrollingSpeed: 700,
-//   autoScrolling: true,
-//   responsiveWidth: 900,
-//   responsiveWidtdh: 800,
-//   fitToSection: true,
-//   fitToSectionDelay: 1000,
-//   scrollBar: false,
-//   easing: "easeInOutCubic",
-//   easingcss3: "ease",
-//   loopBottom: false,
-//   loopTop: false,
-//   loopHorizontal: true,
-//   continuousVertical: false,
-//   continuousHorizontal: false,
-//   scrollHorizontally: false,
-//   interlockedSlides: false,
-//   dragAndMove: false,
-//   offsetSections: false,
-//   resetSliders: false,
-//   fadingEffect: false,
-//   normalScrollElements: "#element1, .element2",
-//   scrollOverflow: false,
-//   scrollOverflowReset: false,
-//   scrollOverflowOptions: null,
-//   touchSensitivity: 15,
-//   bigSectionsDestination: null,
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
 
-//   //Accessibility
-//   keyboardScrolling: true,
-//   animateAnchor: true,
-//   recordHistory: true,
+  links.addEventListener("click", () => toggleMenu());
 
-//   //Custom selectors
-//   sectionSelector: ".section",
-//   slideSelector: ".slide",
+  function toggleMenu() {
+    if (menu.classList.contains("burger-menu_active")) {
+      menu.classList.remove("burger-menu_active");
+      body.style.overflow = "visible";
+    } else {
+      menu.classList.add("burger-menu_active");
+      body.style.overflow = "hidden";
+    }
+  }
+}
 
-//   lazyLoading: true,
-
-//   afterRender: function () {
-//     const mq = window.matchMedia("(min-width: 960px)");
-
-//     if (mq.matches) {
-//       alert("window width >= 960px");
-//       fullpage_api.autoScrolling(true);
-//       fullpage_api.setAllowScrolling(true);
-//     } else {
-//       alert("window width < 960px");
-//       fullpage_api.autoScrolling(false);
-//       fullpage_api.setAllowScrolling(false);
-//     }
-//   },
-// });
+burgerMenu(".nav__burger-menu");
